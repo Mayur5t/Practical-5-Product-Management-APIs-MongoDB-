e40fd22494438a85e26f9062de9b605368078d5c
Mayur5t <pmayur5615@gmail.com>
2021-12-20 20:44:18 +0530

    Initial commit

---
+++ sellerDetails.js
@@ -0,0 +1,14 @@
+var sellerDetails = [ 
+    {
+        "id":"1",
+        "name":"ASTON MARTIN VANTAGE",
+        "productid":["475,965"]
+    },
+    {
+        "id":"2",
+        "name":"ASTON MARTIN BRENTWOOD",
+        "productid":["555,3232"]
+    }
+]
+
+module.exports = sellerDetails;
